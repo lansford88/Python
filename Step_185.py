1545bf8bf70379a4c39e898400b68ea42b29dc8e
lansford88 <rplansford@gmail.com>
2020-08-30 19:05:51 -0700

    Create Step_185.py

---
+++ Step_185.py
@@ -0,0 +1,13 @@
+class gradClass:
+    name = 'No name Provided'
+    email = ' '
+    address = ' '
+    GPA = 0
+
+class Python_oriented(gradClass):
+    Years_Experience = 0
+    base_pay = 35,000
+
+class Availability(gradClass):
+    willing_Remote = ' '
+    contact_Number = 0
